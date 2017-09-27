a0e0eb414115512038a7a52f2806c9d3f0ad1489
ray wang <wangrui@bongmi.com>
2017-09-27 22:39:25 +0800

    attr info interface and factory method

--- ch3/classfile/attrInfo.go
+++ ch3/classfile/attrInfo.go
@@ -1,14 +1,49 @@
-package attribute
-
-import "jvmGo/ch3/classfile"
+package classfile
 
 type AttrInfo interface {
-	ReadInfo(reader classfile.ClassReader)
+	// ReadInfo should read info from the byte stream and constant pool, and returns the should be byte numbers
+	ReadInfo(reader ClassReader, cp ConstantPool) uint64
 }
 
-type AttrInfoBase struct{
-  nameIndex uint16
-  length uint64
+type AttrInfoBase struct {
+	nameIndex uint16
+	length    uint64
 }
 
+const (
+	ATTRNAME_CONSTVALUE                          = "ConstantValue"
+	ATTRNAME_CODE                                = "Code"
+	ATTRNAME_STACKMAPTABLE                       = "StackMapTable" // unsupported
+	ATTRNAME_EXCEPTIONS                          = "Exceptions"
+	ATTRNAME_INNERCLASSES                        = "InnerClasses"
+	ATTRNAME_ECLOSINGMETHOD                      = "EnclosingMethod"
+	ATTRNAME_SYNTHETIC                           = "Synthetic"
+	ATTRNAME_SIGNATURE                           = "Signature"
+	ATTRNAME_SOURCEFILE                          = "SourceFile"
+	ATTRNAME_SOURCEDEBUGEXTENTION                = "SourceDebugExtension"
+	ATTRNAME_LINENUMBERTABLE                     = "LineNumberTable"
+	ATTRNAME_LOCALVARIAVLETABLE                  = "LocalVariableTable"
+	ATTRNAME_LOVALVARIAVLETYPETABLE              = "LocalVariableTypeTable"
+	ATTRNAME_DEPRECATED                          = "Deprecated"                           // unsupported
+	ATTRNAME_RUNTIMEVISIBLEANNOTATIONS           = "RuntimeVisibleAnnotations"            // unsupported
+	ATTRNAME_RUNTIMEINVISIBLEANNOTATIONS         = "RuntimeInvisibleAnnotations"          // unsupported
+	ATTRNAME_RUNTIMEVISIBLEPARAMTERANNOTATIONS   = "RuntimeVisibleParameterAnnotations"   // unsupported
+	ATTRNAME_RUNTIMEINVISIBLEPARAMTERANNOTATIONS = "RuntimeInvisibleParameterAnnotations" // unsupported
+	ATTRNAME_RUNTIMEVISIBLETYPEANNATATIONS       = "RuntimeVisibleTypeAnnotations"        // unsupported
+	ATTRNAME_RUNTIMEINVISIBLETYPEANNATATIONS     = "RuntimeInvisibleTypeAnnotations"      // unsupported
+	ATTRNAME_ANNOTATIONDEFAULT                   = "AnnotationDefault"                    // unsupported
+	ATTRNAME_BOOTSTRAPMETHODS                    = "BootstrapMethods"
+	ATTRNAME_METHODPARAMETERS                    = "MethodParameters"
+)
 
+// factory method for attribute info
+func NewAttributeInfo(reader ClassReader, cp ConstantPool) AttrInfo {
+	nameIndex := reader.ReadUint16()
+	name := cp.getUtf8(nameIndex)
+	switch name {
+	case ATTRNAME_CONSTVALUE:
+		return &AttrConstantValue{}
+	case ATTRNAME_CODE:
+		return &AttrCode{}
+	}
+}
