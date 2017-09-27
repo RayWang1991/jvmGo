4f52a3e3fdcfbab1e902e882f3ac40ccc97d2bf1
ray wang <wangrui@bongmi.com>
2017-09-27 22:40:01 +0800

    constant value

--- ch3/classfile/attr_constantValue.go
+++ ch3/classfile/attr_constantValue.go
@@ -1,1 +1,11 @@
 package classfile
+
+type AttrConstantValue struct {
+	index uint16 // index to constant pool, pointing to a constant value
+}
+
+func (attrConst *AttrConstantValue )ReadInfo (reader ClassReader, cp ConstantPool)uint64{
+	num := reader.ReadUint64()
+	attrConst.index = reader.ReadUint16()
+	return num
+}
