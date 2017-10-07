1af7a8a956b0f7695c756628bc6db7575e568dd5
ray wang <wangrui@bongmi.com>
2017-10-07 23:58:44 +0800

    frame add code and pc

--- ch5/rtdata/frame.go
+++ ch5/rtdata/frame.go
@@ -4,4 +4,7 @@ type Frame struct {
 	LocalVar     LocalVars // the local var represents the local variables, the length is given by compiler
 	OperandStack *OperandStack
 	Next         *Frame
+	// code
+	pc int32
+	code []byte
 }
