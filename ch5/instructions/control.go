98961721d8de1fa54d833fbbfd3b8e35e784f2b7
ray wang <wangrui@bongmi.com>
2017-10-08 23:51:31 +0800

    control

--- ch5/instructions/control.go
+++ ch5/instructions/control.go
@@ -1,1 +1,20 @@
 package instructions
+
+import "jvmGo/ch5/rtdata"
+
+func ggoto(f *rtdata.Frame) {
+	b := f.ReadI16()
+	f.SetPC(f.GetPC() + int32(b))
+}
+
+func ggoto_w(f *rtdata.Frame) {
+	b := f.ReadI32()
+	f.SetPC(f.GetPC() + b)
+}
+
+// jump subroutine
+func jsr(f *rtdata.Frame) {
+	addr := f.ReadI16()
+	
+
+}
