91c635d7571beebd731310d6d941be556fa38c59
ray wang <wangrui@bongmi.com>
2017-10-10 13:41:17 +0800

    test generic, Local var type table

---
+++ ClassFileTest/GenericTest.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenericTest {
+  public static void main(String[] args) {
+    List<String> list = new ArrayList<String>();
+    list.add("123");
+  }
+}
