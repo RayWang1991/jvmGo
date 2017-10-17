ae300c439d8484bf5fae239dff265371175f774c
ray wang <wangrui@bongmi.com>
2017-10-17 10:17:43 +0800

    update error code

--- ch6/utils/errcode.go
+++ ch6/utils/errcode.go
@@ -1,9 +1,18 @@
 package utils
 
 const (
-	OutOfMemoryError  = "OutOfMemoryError"
-	DivideByZero      = "DivideByZero"
-	StackOverFlow     = "StackOverFlow"
-	ClassMagicError   = "java.lang.ClassFormatError:magic!"
-	ClassVersionError = "java.lang.UnsupportedClassVersionError!"
+	OutOfMemoryError             = "OutOfMemoryError"
+	DivideByZero                 = "DivideByZero"
+	StackOverFlow                = "StackOverFlow"
+	ClassMagicError              = "java.lang.ClassFormatError:magic!"
+	ClassVersionError            = "java.lang.UnsupportedClassVersionError!"
+	LinkageError                 = "java.lang.LinkageError"
+	ClassNotFoundException       = "java.lang.ClassNotFoundException"
+	ClassFormatError             = "java.lang.ClassFormatError"
+	NoSuchFieldError             = "java.lang.NoSuchFieldError"
+	IllegalAccessError           = "java.lang.IllegalAccessError"
+	IncompatibleClassChangeError = "java.lang.IncompatibleClassChangeError"
+	NullPointerException         = "java.lang.NullPointerException"
+	InstantiationError           = "java.lang.InstantiationError"
+	ClassCastException           = "java.lang.ClassCastException"
 )
