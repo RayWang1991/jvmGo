aadb1ab712d7fd1861fca575177f1c35c715da79
ray wang <wangrui@bongmi.com>
2017-10-18 22:56:36 +0800

    class loader constants

--- ch6/marea/classLoader.go
+++ ch6/marea/classLoader.go
@@ -1,1 +1,15 @@
 package marea
+
+type ClassLoader interface {
+	ID() int
+	Delegate() ClassLoader
+	Initiate(n string) *Class
+	Define(n string) *Class
+	Verify(class *Class)
+	Prepare(class *Class)
+}
+
+const (
+	BootstrapClassLoaderId   = iota
+	UserDefinedClassLoaderId
+)
