f86c03b7b4c698eb1e5025ca803c156553353757
ray wang <wangrui@bongmi.com>
2017-10-19 22:54:36 +0800

    update interpreter

--- ch6/inptr.go
+++ ch6/inptr.go
@@ -2,19 +2,16 @@ package main
 
 import (
 	"fmt"
-	"jvmGo/ch6/classfile"
 	"jvmGo/ch6/instructions"
 	"jvmGo/ch6/rtdt"
 	"jvmGo/ch6/cmn"
+	"jvmGo/ch6/marea"
+	"jvmGo/ch6/classfile"
 )
 
-func interpret(m *classfile.MethodInfo) {
-	codeAttr := m.GetCodeAttr()
-	maxStackDep := uint(codeAttr.MaxStack())
-	maxLvals := uint(codeAttr.MaxLocals())
-	code := codeAttr.Code()
+func interpret(m *marea.Method) {
 	thread := rtdt.NewThread(1024)
-	frame := rtdt.NewFrame(maxLvals, maxStackDep, code)
+	frame := rtdt.NewFrame(m)
 	thread.PushFrame(frame)
 	defer catchErr(thread)
 	loop(thread)
@@ -29,6 +26,8 @@ func catchErr(t *rtdt.Thread) {
 // loop for current frame
 func loop(t *rtdt.Thread) {
 	f := t.CurrentFrame()
+	fmt.Printf("enter func %s %s\n", f.Method().Name(), f.Method().Desc())
+	fmt.Print(classfile.CodeInst(f.Method().Code()).String())
 	for {
 		code := f.ReadU8() // read next opcode
 		fmt.Printf("pc:%-4d code:%s\n", f.GetPC()-1, cmn.InstStr(code))
