fa5e2e6fb34cf63b2fa43a1b415afebcaaee4f58
ray wang <wangrui@bongmi.com>
2017-10-22 21:22:17 +0800

    update, add Thread field, new func, change code field into method field

--- ch6/rtdt/frame.go
+++ ch6/rtdt/frame.go
@@ -1,25 +1,40 @@
 package rtdt
 
 import (
-	data "jvmGo/ch6/marea"
+	"jvmGo/ch6/marea"
+	"fmt"
 )
 
 type Frame struct {
-	LocalVar     data.Vars // the local var represents the local variables, the length is given by compiler
+	method       *marea.Method
+	LocalVar     marea.Vars // the local var represents the local variables, the length is given by compiler
 	OperandStack *OperandStack
-	next         *Frame
-	// code
-	pc   int32 // for code read and execute
-	code []byte
+
+	thread *Thread
+	next   *Frame
+	pc     int32 // for code read and execute
 }
 
 // new
-func NewFrame(maxLocals, maxOperands uint, code []byte) *Frame {
+func NewFrame(method *marea.Method, t *Thread) *Frame {
+	fmt.Println("New Frame")
+	fmt.Println("MaxStack:", method.MaxStackDep())
 	return &Frame{
-		LocalVar:     data.NewLocalVars(maxLocals),
-		OperandStack: NewOperandStack(maxOperands),
-		code:         code,
+		method:       method,
+		LocalVar:     marea.NewVars(uint(method.MaxLocalVars())),
+		OperandStack: NewOperandStack(uint(method.MaxStackDep())),
+		thread:       t,
 	}
+}
+
+// thread
+func (f *Frame) Thread() *Thread {
+	return f.thread
+}
+
+// method
+func (c *Frame) Method() *marea.Method {
+	return c.method
 }
 
 // next
@@ -41,14 +56,14 @@ func (c *Frame) SetPC(pc int32) {
 }
 
 func (c *Frame) ReadU8() byte {
-	r := c.code[c.pc]
+	r := c.method.Code()[c.pc]
 	c.pc++
 	return r
 }
 
 // read two code(u8) as u16
 func (c *Frame) ReadU16() uint16 {
-	high, low := c.code[c.pc], c.code[c.pc+1]
+	high, low := c.method.Code()[c.pc], c.method.Code()[c.pc+1]
 	c.pc += 2
 	return uint16(high)<<8 | uint16(low)
 }
@@ -58,10 +73,10 @@ func (c *Frame) ReadI16() int16 {
 }
 
 func (c *Frame) ReadI32() int32 {
-	b0 := int32(c.code[c.pc])
-	b1 := int32(c.code[c.pc+1])
-	b2 := int32(c.code[c.pc+2])
-	b3 := int32(c.code[c.pc+3])
+	b0 := int32(c.method.Code()[c.pc])
+	b1 := int32(c.method.Code()[c.pc+1])
+	b2 := int32(c.method.Code()[c.pc+2])
+	b3 := int32(c.method.Code()[c.pc+3])
 	c.pc += 4
 	return b0<<24 | b1<<16 | b2<<8 | b3
 }
