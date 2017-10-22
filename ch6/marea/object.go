63c41ee66985e0bd9c37a24cad267dfa20863662
ray wang <wangrui@bongmi.com>
2017-10-22 21:22:46 +0800

    update, add class and vars fields

--- ch6/marea/object.go
+++ ch6/marea/object.go
@@ -1,7 +1,19 @@
 package marea
 
-// An Object represents a general reference type, for class type, interface type, and array type
+// An Object represents a general reference type, for from type, interface type, and array type
 type Object struct {
-	// TODO
 	class *Class
+	Vars
+}
+
+func NewObject(class *Class) *Object {
+	vs := NewVars(class.InsSlotNum())
+	return &Object{
+		class: class,
+		Vars:  vs,
+	}
+}
+
+func (o *Object) Class() *Class {
+	return o.class
 }
