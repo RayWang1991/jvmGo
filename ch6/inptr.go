29eae64d56731a9b273147f31e5611678855647a
ray wang <wangrui@bongmi.com>
2017-10-23 20:15:48 +0800

    change interpreter’s loop, for method invoke and return

--- ch6/inptr.go
+++ ch6/inptr.go
@@ -2,18 +2,17 @@ package main
 
 import (
 	"fmt"
-	"jvmGo/ch6/instructions"
-	"jvmGo/ch6/rtdt"
 	"jvmGo/ch6/cmn"
+	"jvmGo/ch6/instructions"
 	"jvmGo/ch6/marea"
-	"jvmGo/ch6/classfile"
+	"jvmGo/ch6/rtdt"
 )
 
 func interpret(m *marea.Method) {
 	thread := rtdt.NewThread(1024)
-	frame := rtdt.NewFrame(m)
+	frame := rtdt.NewFrame(m, thread)
 	thread.PushFrame(frame)
-	defer catchErr(thread)
+	//defer catchErr(thread)
 	loop(thread)
 }
 func catchErr(t *rtdt.Thread) {
@@ -25,13 +24,20 @@ func catchErr(t *rtdt.Thread) {
 
 // loop for current frame
 func loop(t *rtdt.Thread) {
-	f := t.CurrentFrame()
-	fmt.Printf("enter func %s %s\n", f.Method().Name(), f.Method().Desc())
-	fmt.Print(classfile.CodeInst(f.Method().Code()).String())
-	for {
-		code := f.ReadU8() // read next opcode
+	fmt.Println()
+	var f = t.CurrentFrame()
+	for ; f != nil; f = t.CurrentFrame() {
+		if f == nil {
+			fmt.Println("exit")
+			break
+		}
+		//fmt.Printf("enter func %s %s\n", f.Method().Name(), f.Method().Desc())
+		//fmt.Print(classfile.CodeInst(f.Method().Code()).String())
+		code := f.ReadU8() // read next opcode/**/
 		fmt.Printf("pc:%-4d code:%s\n", f.GetPC()-1, cmn.InstStr(code))
 		fn := instructions.InstFnc(code)
 		fn(f)
+		//fmt.Printf("Vars:%v\n", t.CurrentFrame().LocalVar)
+		//fmt.Printf("OperandStack:%v\n", t.CurrentFrame().OperandStack)
 	}
 }
