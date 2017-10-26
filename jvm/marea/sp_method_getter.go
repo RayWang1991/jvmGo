fc50c2330dd74271c7488504e64170d15c70951e
ray wang <wangrui@bongmi.com>
2017-10-26 23:13:30 +0800

    void hard code

--- jvm/marea/sp_method_getter.go
+++ jvm/marea/sp_method_getter.go
@@ -1,9 +1,11 @@
 package marea
 
+import "jvmGo/jvm/cmn"
+
 func (c *Class) GetMain() *Method {
-	return c.GetMethodDirect("main", "([Ljava/lang/String;)V")
+	return c.GetMethodDirect(cmn.METHOD_MAIN_NAME, cmn.METHOD_MAIN_DESC)
 }
 
-func (c *Class) GetCInit() *Method {
-	return c.GetMethodDirect("<clinit>", "()V")
+func (c *Class) GetClinit() *Method {
+	return c.GetMethodDirect(cmn.METHOD_CLINIT_NAME, cmn.METHOD_CLINIT_DESC)
 }
