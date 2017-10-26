fb829f7e0873a06a65a69be8d639277e8768b512
ray wang <wangrui@bongmi.com>
2017-10-26 23:33:13 +0800

    array load and store

--- jvm/instructions/loadAndStore.go
+++ jvm/instructions/loadAndStore.go
@@ -1,6 +1,11 @@
 package instructions
 
-import "jvmGo/jvm/rtdt"
+import (
+	"jvmGo/jvm/cmn"
+	"jvmGo/jvm/marea"
+	"jvmGo/jvm/rtdt"
+	"jvmGo/jvm/utils"
+)
 
 // Loads
 // iload
@@ -100,24 +105,130 @@ func aload(f *rtdt.Frame) {
 	v := f.LocalVar.GetRef(uint(i))
 	f.OperandStack.PushRef(v)
 }
+
 func aload_0(f *rtdt.Frame) {
 	v := f.LocalVar.GetRef(uint(0))
 	f.OperandStack.PushRef(v)
 }
+
 func aload_1(f *rtdt.Frame) {
 	v := f.LocalVar.GetRef(uint(1))
 	f.OperandStack.PushRef(v)
 }
+
 func aload_2(f *rtdt.Frame) {
 	v := f.LocalVar.GetRef(uint(2))
 	f.OperandStack.PushRef(v)
 }
+
 func aload_3(f *rtdt.Frame) {
 	v := f.LocalVar.GetRef(uint(3))
 	f.OperandStack.PushRef(v)
 }
 
-// TODO load int,float,refs... from array
+func iaload(f *rtdt.Frame) {
+	indx := f.OperandStack.PopInt()
+	arref := f.OperandStack.PopRef()
+	if arref == nil {
+		panic(utils.NullPointerException)
+	}
+	length := arref.ArrayLengthI()
+	if indx < 0 || indx > length-1 {
+		panic(utils.ArrayIndexOutOfBoundsException)
+	}
+	f.OperandStack.PushInt(arref.ArrGetInts()[indx])
+}
+
+func laload(f *rtdt.Frame) {
+	indx := f.OperandStack.PopInt()
+	arref := f.OperandStack.PopRef()
+	if arref == nil {
+		panic(utils.NullPointerException)
+	}
+	length := arref.ArrayLengthJ()
+	if indx < 0 || indx > length-1 {
+		panic(utils.ArrayIndexOutOfBoundsException)
+	}
+	f.OperandStack.PushLong(arref.ArrGetLongs()[indx])
+}
+
+func faload(f *rtdt.Frame) {
+	indx := f.OperandStack.PopInt()
+	arref := f.OperandStack.PopRef()
+	if arref == nil {
+		panic(utils.NullPointerException)
+	}
+	length := arref.ArrayLengthF()
+	if indx < 0 || indx > length-1 {
+		panic(utils.ArrayIndexOutOfBoundsException)
+	}
+	f.OperandStack.PushFloat(arref.ArrGetFloats()[indx])
+}
+
+func daload(f *rtdt.Frame) {
+	indx := f.OperandStack.PopInt()
+	arref := f.OperandStack.PopRef()
+	if arref == nil {
+		panic(utils.NullPointerException)
+	}
+	length := arref.ArrayLengthD()
+	if indx < 0 || indx > length-1 {
+		panic(utils.ArrayIndexOutOfBoundsException)
+	}
+	f.OperandStack.PushDouble(arref.ArrGetDoubles()[indx])
+}
+
+func aaload(f *rtdt.Frame) {
+	indx := f.OperandStack.PopInt()
+	arref := f.OperandStack.PopRef()
+	if arref == nil {
+		panic(utils.NullPointerException)
+	}
+	length := arref.ArrayLengthA()
+	if indx < 0 || indx > length-1 {
+		panic(utils.ArrayIndexOutOfBoundsException)
+	}
+	f.OperandStack.PushRef(arref.ArrGetRefs()[indx])
+}
+
+func baload(f *rtdt.Frame) {
+	indx := f.OperandStack.PopInt()
+	arref := f.OperandStack.PopRef()
+	if arref == nil {
+		panic(utils.NullPointerException)
+	}
+	length := arref.ArrayLengthB()
+	if indx < 0 || indx > length-1 {
+		panic(utils.ArrayIndexOutOfBoundsException)
+	}
+	f.OperandStack.PushInt(int32(arref.ArrGetBytes()[indx]))
+}
+
+func caload(f *rtdt.Frame) {
+	indx := f.OperandStack.PopInt()
+	arref := f.OperandStack.PopRef()
+	if arref == nil {
+		panic(utils.NullPointerException)
+	}
+	length := arref.ArrayLengthC()
+	if indx < 0 || indx > length-1 {
+		panic(utils.ArrayIndexOutOfBoundsException)
+	}
+	f.OperandStack.PushInt(int32(arref.ArrGetChars()[indx]))
+}
+
+func saload(f *rtdt.Frame) {
+	indx := f.OperandStack.PopInt()
+	arref := f.OperandStack.PopRef()
+	if arref == nil {
+		panic(utils.NullPointerException)
+	}
+	length := arref.ArrayLengthS()
+	if indx < 0 || indx > length-1 {
+		panic(utils.ArrayIndexOutOfBoundsException)
+	}
+	f.OperandStack.PushInt(int32(arref.ArrGetShorts()[indx]))
+}
 
 // Stores
 func istore(f *rtdt.Frame) {
@@ -230,4 +341,151 @@ func lstore_3(f *rtdt.Frame) {
 	f.LocalVar.SetLong(r, 3)
 }
 
-// TODO <T>a, fa,la,ia... store, store T to array
+func iastore(f *rtdt.Frame) {
+	val := f.OperandStack.PopInt()
+	index := f.OperandStack.PopInt()
+	arrayRef := f.OperandStack.PopRef()
+	if arrayRef == nil {
+		panic(utils.NullPointerException)
+	}
+	arrayLen := arrayRef.ArrayLengthJ()
+	if index < 0 || index+1 > arrayLen {
+		panic(utils.ArrayIndexOutOfBoundsException)
+	}
+	cn := arrayRef.Class().ClassName()
+	if cn != "[I" {
+		panic(utils.ArrayStoreException)
+	}
+	arrayRef.ArrGetInts()[index] = val
+}
+
+func lastore(f *rtdt.Frame) {
+	val := f.OperandStack.PopLong()
+	index := f.OperandStack.PopInt()
+	arrayRef := f.OperandStack.PopRef()
+	if arrayRef == nil {
+		panic(utils.NullPointerException)
+	}
+	arrayLen := arrayRef.ArrayLengthJ()
+	if index < 0 || index+1 > arrayLen {
+		panic(utils.ArrayIndexOutOfBoundsException)
+	}
+	cn := arrayRef.Class().ClassName()
+	if cn != "[J" {
+		panic(utils.ArrayStoreException)
+	}
+	arrayRef.ArrGetLongs()[index] = val
+}
+
+func fastore(f *rtdt.Frame) {
+	val := f.OperandStack.PopFloat()
+	index := f.OperandStack.PopInt()
+	arrayRef := f.OperandStack.PopRef()
+	if arrayRef == nil {
+		panic(utils.NullPointerException)
+	}
+	arrayLen := arrayRef.ArrayLengthF()
+	if index < 0 || index+1 > arrayLen {
+		panic(utils.ArrayIndexOutOfBoundsException)
+	}
+	cn := arrayRef.Class().ClassName()
+	if cn != "[F" {
+		panic(utils.ArrayStoreException)
+	}
+	arrayRef.ArrGetFloats()[index] = val
+}
+
+func dastore(f *rtdt.Frame) {
+	val := f.OperandStack.PopDouble()
+	index := f.OperandStack.PopInt()
+	arrayRef := f.OperandStack.PopRef()
+	if arrayRef == nil {
+		panic(utils.NullPointerException)
+	}
+	arrayLen := arrayRef.ArrayLengthD()
+	if index < 0 || index+1 > arrayLen {
+		panic(utils.ArrayIndexOutOfBoundsException)
+	}
+	cn := arrayRef.Class().ClassName()
+	if cn != "[D" {
+		panic(utils.ArrayStoreException)
+	}
+	arrayRef.ArrGetDoubles()[index] = val
+}
+
+func aastore(f *rtdt.Frame) {
+	val := f.OperandStack.PopRef()
+	if val == nil {
+		panic(utils.NullPointerException)
+	}
+	index := f.OperandStack.PopInt()
+	arrayRef := f.OperandStack.PopRef()
+	if arrayRef == nil {
+		panic(utils.NullPointerException)
+	}
+	arrayLen := arrayRef.ArrayLengthA()
+	if index < 0 || index+1 > arrayLen {
+		panic(utils.ArrayIndexOutOfBoundsException)
+	}
+	arrC := arrayRef.Class()
+	eleN := cmn.ElementName(arrC.ClassName())
+	eleC := arrC.DefineLoader().Load(eleN) // must not be primitive type
+	if !marea.IsAssignable(val.Class(), eleC) {
+		panic(utils.ArrayStoreException)
+	}
+	arrayRef.ArrGetRefs()[index] = val
+}
+
+func bastore(f *rtdt.Frame) {
+	val := int8(f.OperandStack.PopInt())
+	index := f.OperandStack.PopInt()
+	arrayRef := f.OperandStack.PopRef()
+	if arrayRef == nil {
+		panic(utils.NullPointerException)
+	}
+	arrayLen := arrayRef.ArrayLengthB()
+	if index < 0 || index+1 > arrayLen {
+		panic(utils.ArrayIndexOutOfBoundsException)
+	}
+	cn := arrayRef.Class().ClassName()
+	if cn != "[B" && cn != "[Z" {
+		panic(utils.ArrayStoreException)
+	}
+	arrayRef.ArrGetBytes()[index] = val
+}
+
+func castore(f *rtdt.Frame) {
+	val := uint16(f.OperandStack.PopInt())
+	index := f.OperandStack.PopInt()
+	arrayRef := f.OperandStack.PopRef()
+	if arrayRef == nil {
+		panic(utils.NullPointerException)
+	}
+	arrayLen := arrayRef.ArrayLengthC()
+	if index < 0 || index+1 > arrayLen {
+		panic(utils.ArrayIndexOutOfBoundsException)
+	}
+	cn := arrayRef.Class().ClassName()
+	if cn != "[C" {
+		panic(utils.ArrayStoreException)
+	}
+	arrayRef.ArrGetChars()[index] = val
+}
+
+func sastore(f *rtdt.Frame) {
+	val := int16(f.OperandStack.PopInt())
+	index := f.OperandStack.PopInt()
+	arrayRef := f.OperandStack.PopRef()
+	if arrayRef == nil {
+		panic(utils.NullPointerException)
+	}
+	arrayLen := arrayRef.ArrayLengthS()
+	if index < 0 || index+1 > arrayLen {
+		panic(utils.ArrayIndexOutOfBoundsException)
+	}
+	cn := arrayRef.Class().ClassName()
+	if cn != "[S" {
+		panic(utils.ArrayStoreException)
+	}
+	arrayRef.ArrGetShorts()[index] = val
+}
