3f2b56f27274da7e3bf7e35389319749c49f041c
ray wang <wangrui@bongmi.com>
2017-10-26 23:12:15 +0800

    add cloneable and serializable

--- jvm/utils/hardCode.go
+++ jvm/utils/hardCode.go
@@ -1,5 +1,8 @@
 package utils
 
 const (
-	CLASSNAME_Object = "java/lang/Object"
+	CLASSNAME_Object      = "java/lang/Object"
+	CLASSNAME_System      = "java/lang/System"
+	CLASSNAME_Cloneable   = "java/io/Cloneable"
+	CLASSNAME_Serilizable = "java/lang/Serilizable"
 )
