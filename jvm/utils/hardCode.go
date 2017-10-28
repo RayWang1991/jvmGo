0ef8f6f126be0dc8feac154558f9d10db30412b0
ray wang <wangrui@bongmi.com>
2017-10-28 11:10:32 +0800

    fix typo

--- jvm/utils/hardCode.go
+++ jvm/utils/hardCode.go
@@ -1,8 +1,8 @@
 package utils
 
 const (
-	CLASSNAME_Object      = "java/lang/Object"
-	CLASSNAME_System      = "java/lang/System"
-	CLASSNAME_Cloneable   = "java/io/Cloneable"
-	CLASSNAME_Serilizable = "java/lang/Serilizable"
+	CLASSNAME_Object       = "java/lang/Object"
+	CLASSNAME_System       = "java/lang/System"
+	CLASSNAME_Cloneable    = "java/lang/Cloneable"
+	CLASSNAME_Serializable = "java/io/Serializable"
 )
