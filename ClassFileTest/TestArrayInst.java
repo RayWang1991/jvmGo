630422353d47a8ec07edc0dc7de55668c902377f
ray wang <wangrui@bongmi.com>
2017-10-28 11:11:53 +0800

    update, test multiple array

--- ClassFileTest/TestArrayInst.java
+++ ClassFileTest/TestArrayInst.java
@@ -3,9 +3,12 @@ public class TestArrayInst {
     boolean[] bArr = {true, false, true}; // new array b
     bArr[1] = bArr[2]; //bastore; baload
     long[] lArr = {1024, 1000000000000L, 0} ;// new array J
-    lArr[1] = lArr[2] ;//lastore; laload
+    lArr[2] = lArr[1] ;//lastore; laload
     double[] dArr = {102.4, 100.0000000000,  0.1} ;// new array D
     dArr[1] = dArr[2] ;//dastore; daload
+    
+    int[][][] iArr = new int[1][2][3];
+    iArr[0][1][1] = 100;
     
 //    Object[][][] oArr = new Object[3][4][5] ;// multianewarray 
 //    oArr[1][2][3] = new Object();
