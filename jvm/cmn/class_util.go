df94e8eea71f8817eba157a49f287048c0ad051e
ray wang <wangrui@bongmi.com>
2017-10-29 16:22:08 +0800

    add support for primitive types

--- jvm/cmn/class_util.go
+++ jvm/cmn/class_util.go
@@ -1,7 +1,46 @@
 package cmn
 
-import "strings"
+import (
+	"strings"
+)
 
 func IsArray(name string) bool {
-	return strings.HasSuffix(name, "[")
+	return strings.HasPrefix(name, "[")
+}
+
+func ElementName(name string) string { // ignore '[
+	return name[1:]
+}
+
+func IsPrimitiveType(name string) bool {
+	if len(name) != 1 {
+		return false
+	}
+	c := name[0]
+	switch c {
+	case 'Z', 'B', 'C', 'S', 'F', 'D', 'I', 'J':
+		return true
+	default:
+		return false
+	}
+
+	if name[0] == '[' || name[0] == 'L' {
+		return false
+	} else {
+		return true
+	}
+}
+
+// for array class name to element the L and ;
+func SimClassName(name string) string {
+	if len(name) == 0 || name[0] != '[' {
+		return name
+	}
+	// element first L
+	i := strings.IndexByte(name, 'L')
+	if i < 0 {
+		// not find L
+		return name
+	}
+	return name[:i] + name[i+1:len(name)-1]
 }
