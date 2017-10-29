71fedc93ef761e30776f3982be3c3dbd9bd645a1
ray wang <wangrui@bongmi.com>
2017-10-29 16:22:18 +0800

    update

--- jvm/cmn/method_util.go
+++ jvm/cmn/method_util.go
@@ -1,1 +1,10 @@
 package cmn
+
+const (
+	METHOD_MAIN_NAME                  = "main"
+	METHOD_MAIN_DESC                  = "([Ljava/lang/String;)V"
+	METHOD_CLINIT_NAME                = "<clinit>"
+	METHOD_CLINIT_DESC                = "()V"
+	METHOD_initializeSystemClass_NAME = "initializeSystemClass"
+	METHOD_initializeSystemClass_DESC = "()V"
+)
