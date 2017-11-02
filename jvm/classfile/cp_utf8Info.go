33b3af9d63ce9595f862dc24da5808a004602dc4
ray wang <wangrui@bongmi.com>
2017-11-02 23:04:25 +0800

    change string encode/decode inc pos

--- jvm/classfile/cp_utf8Info.go
+++ jvm/classfile/cp_utf8Info.go
@@ -1,5 +1,7 @@
 package classfile
 
+import "jvmGo/jvm/cmn"
+
 type Utf8Info struct {
 	val string // []byte data
 }
@@ -7,7 +9,7 @@ type Utf8Info struct {
 func (u *Utf8Info) ReadInfo(reader *ClassReader) {
 	length := reader.ReadUint16() // length for the utf8 info in bytes
 	bs := reader.ReadBytes(uint(length))
-	u.val = decodeMUTF8(bs)
+	u.val = cmn.MUTF8ToUTF8(bs)
 }
 
 func (u *Utf8Info) String() string {
