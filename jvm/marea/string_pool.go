b9fb92eb615a80162555d842d0bf5d81282e06f4
ray wang <wangrui@bongmi.com>
2017-11-02 23:03:43 +0800

    add string pool

--- jvm/marea/string_pool.go
+++ jvm/marea/string_pool.go
@@ -1,1 +1,28 @@
 package marea
+
+import "jvmGo/jvm/cmn"
+
+var strPool = make(map[string]*Object)
+
+func GetJavaString(key string, loader ClassLoader) *Object {
+	if v := strPool[key]; v != nil {
+		return v
+	}
+	sClass := loader.Load("java/lang/String")
+	csClass := loader.Load("[C")
+
+	craw := cmn.UTF8ToUTF16(key)
+	c := NewArrayC(csClass, int32(len(craw)))
+
+	ca := c.ArrGetChars()
+	for i := range ca {
+		ca[i] = craw[i]
+	}
+
+	s:= NewObject(sClass)
+	sf := sClass.GetFieldDirect("value", "[C")
+	s.SetRef(c, sf.VarIdx())
+
+	strPool[key] = s
+	return s
+}
