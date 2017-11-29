7a831911be9a8c89a828ce9c682f9e674b7be1be
ray wang <wangrui@bongmi.com>
2017-11-29 11:13:07 +0800

    vm native

--- jvm/lib/vm.go
+++ jvm/lib/vm.go
@@ -1,1 +1,18 @@
 package lib
+
+import (
+	"jvmGo/jvm/rtdt"
+	"jvmGo/jvm/utils"
+)
+
+func init() {
+	register(utils.CLASSNAME_System, utils.METHODNAME_InitializeSystemClass, "()V", initialize)
+}
+
+func initialize(frame *rtdt.Frame) {
+	panic("")
+	loader := frame.Method().Class().DefineLoader()
+	sysC := loader.Load(utils.CLASSNAME_System)
+	initM := sysC.GetMethodDirect(utils.METHODNAME_InitializeSystemClass, "()V")
+	callMethod(initM, frame)
+}
