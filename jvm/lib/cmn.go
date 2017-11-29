39b109b0d821080c83fca1d063661bc227962bf5
ray wang <wangrui@bongmi.com>
2017-11-29 11:12:28 +0800

    update

--- jvm/lib/cmn.go
+++ jvm/lib/cmn.go
@@ -1,1 +1,20 @@
 package lib
+
+import (
+	"jvmGo/jvm/marea"
+	"jvmGo/jvm/rtdt"
+)
+
+func callMethod(m *marea.Method, f *rtdt.Frame) {
+	t := f.Thread()
+	nf := rtdt.NewFrame(m, t)
+	t.PushFrame(nf)
+	i := m.ArgSlotNum()
+	if m.IsStatic() {
+		i--
+	}
+	for ; i >= 0; i-- {
+		slot := f.OperandStack.PopSlot()
+		nf.LocalVar.SetSlot(slot, uint(i))
+	}
+}
