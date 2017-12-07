d331b2f964d6d262c2bba38786fbb118d9a99a7f
ray wang <wangrui@bongmi.com>
2017-12-07 18:52:55 +0800

    vm initialize call Class.initializeSystemClass

--- jvm/lib/vm.go
+++ jvm/lib/vm.go
@@ -6,11 +6,10 @@ import (
 )
 
 func init() {
-	register(utils.CLASSNAME_System, utils.METHODNAME_InitializeSystemClass, "()V", initialize)
+	register(utils.CLASSNAME_VM, utils.METHODNAME_Initialize, "()V", initialize)
 }
 
 func initialize(frame *rtdt.Frame) {
-	panic("")
 	loader := frame.Method().Class().DefineLoader()
 	sysC := loader.Load(utils.CLASSNAME_System)
 	initM := sysC.GetMethodDirect(utils.METHODNAME_InitializeSystemClass, "()V")
