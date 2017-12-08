87298efb0ffb5c4c0228670a23f6c4c7c6eede97
ray wang <wangrui@bongmi.com>
2017-12-08 22:40:49 +0800

    add dummy frame for native methods invoke

--- jvm/lib/base.go
+++ jvm/lib/base.go
@@ -3,6 +3,7 @@ package lib
 import (
 	"jvmGo/jvm/marea"
 	"jvmGo/jvm/rtdt"
+	"jvmGo/jvm/cmn"
 )
 
 func callMethod(m *marea.Method, f *rtdt.Frame) {
@@ -17,4 +18,16 @@ func callMethod(m *marea.Method, f *rtdt.Frame) {
 		slot := f.OperandStack.PopSlot()
 		nf.LocalVar.SetSlot(slot, uint(i))
 	}
+}
+
+func dummyFrame(op *rtdt.OperandStack, t *rtdt.Thread) *rtdt.Frame {
+	dummyClass := marea.HackClass("<dummy>")
+	m := marea.HackMethod(
+		dummyClass,
+		cmn.ACC_STATIC,
+		"<dummy>",
+		"()V",
+		[]byte{cmn.OPCODE_rreturn})
+	frame := rtdt.HackFrame(m, t, op, nil)
+	return frame
 }
