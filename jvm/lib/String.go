91e69f51b21ea7365215f795889764c33a5ccf29
ray wang <wangrui@bongmi.com>
2017-12-13 22:00:25 +0800

    intern

--- jvm/lib/String.go
+++ jvm/lib/String.go
@@ -1,1 +1,19 @@
 package lib
+
+import (
+	"jvmGo/jvm/rtdt"
+	"jvmGo/jvm/marea"
+	"jvmGo/jvm/utils"
+)
+
+func init() {
+	register(utils.CLASSNAME_String, "intern", "()Ljava/lang/String;", intern)
+}
+
+// public native String intern()
+// ()Ljava/lang/String;
+func intern(f *rtdt.Frame) {
+	this := f.LocalVar.GetRef(0)
+	internStr := marea.GetInternString(this)
+	f.OperandStack.PushRef(internStr)
+}
