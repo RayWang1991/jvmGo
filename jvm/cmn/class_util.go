5a5cc9ae14b71e66f4d95fa3201c4ab45cc335cc
ray wang <wangrui@bongmi.com>
2017-12-16 22:25:53 +0800

    parse description to Class name

--- jvm/cmn/class_util.go
+++ jvm/cmn/class_util.go
@@ -2,6 +2,7 @@ package cmn
 
 import (
 	"strings"
+	"jvmGo/jvm/utils"
 )
 
 func IsArray(name string) bool {
@@ -31,7 +32,7 @@ func IsPrimitiveType(name string) bool {
 	}
 }
 
-// for array class name to element the L and ;
+// for array class name to delete the L and ;
 func SimClassName(name string) string {
 	if len(name) == 0 || name[0] != '[' {
 		return name
@@ -51,4 +52,35 @@ func ToDoted(str string) string {
 
 func ToSlash(str string) string {
 	return strings.Replace(str, ".", "/", -1)
+}
+
+func ToClassName(desc string) string {
+	if desc[0] == 'L' {
+		return desc[1:len(desc)-1]
+	}
+	if desc[0] == '[' {
+		return "[" + ToClassName(desc[1:])
+	}
+	if len(desc) == 1 {
+		switch desc[0] {
+		case 'Z':
+			return utils.CLASSNAME_prim_boolean
+		case 'B':
+			return utils.CLASSNAME_prim_byte
+		case 'C':
+			return utils.CLASSNAME_prim_char
+		case 'S':
+			return utils.CLASSNAME_prim_short
+		case 'F':
+			return utils.CLASSNAME_prim_float
+		case 'D':
+			return utils.CLASSNAME_prim_double
+		case 'I':
+			return utils.CLASSNAME_prim_int
+		case 'J':
+			return utils.CLASSNAME_prim_long
+		}
+	}
+	// must be
+	panic("unsupported desc" + desc)
 }
