fded4e112ea4158d03f2344819f87770469cff28
ray wang <wangrui@bongmi.com>
2017-12-18 22:29:09 +0800

    compareAndSwapInt, getIntVolatile

--- jvm/lib/Unsafe.go
+++ jvm/lib/Unsafe.go
@@ -14,6 +14,8 @@ func init() {
 	register(utils.CLASSNAME_Unsafe, "addressSize", "()I", addressSize)
 	register(utils.CLASSNAME_Unsafe, "objectFieldOffset", "(Ljava/lang/reflect/Field;)J", objectFieldOffset)
 	register(utils.CLASSNAME_Unsafe, "compareAndSwapObject", "(Ljava/lang/Object;JLjava/lang/Object;Ljava/lang/Object;)Z", compareAndSwapObject)
+	register(utils.CLASSNAME_Unsafe, "compareAndSwapInt", "(Ljava/lang/Object;JII)Z", compareAndSwapInt)
+	register(utils.CLASSNAME_Unsafe, "getIntVolatile", "(Ljava/lang/Object;J)I", getIntVolatile)
 }
 
 // public native int arrayBaseOffset(Class<?> type);
@@ -46,8 +48,9 @@ func addressSize(frame *rtdt.Frame) {
 // (Ljava/lang/reflect/Field;)J
 func objectFieldOffset(f *rtdt.Frame) {
 	jField := f.LocalVar.GetRef(1)
-	slot := jField.GetInsFieldSlotIdx("slot")
-	f.OperandStack.PushLong(int64(slot))
+	slotIdx := jField.GetInsFieldSlotIdx("slot")
+	slotN := jField.GetInt(slotIdx)
+	f.OperandStack.PushLong(int64(slotN))
 }
 
 // public final native boolean compareAndSwapObject(Object o, long offset, Object expected, Object x)
@@ -76,6 +79,60 @@ func compareAndSwapObject(f *rtdt.Frame) {
 			f.OperandStack.PushInt(0)
 		}
 	default: //todo
-		panic("unsupported yet")
+		panic(fmt.Errorf("compareAndSwapObject, wrong type %T!", data))
+	}
+}
+
+// public final native boolean compareAndSwapInt(object ar1, long var2 ,int var4, int var5);
+// (Ljava/lang/Object;JII)Z
+func compareAndSwapInt(f *rtdt.Frame) {
+	vars := f.LocalVar
+	obj := vars.GetRef(1)
+	offset := vars.GetLong(2)
+	off := uint(uint32(offset))
+	expected := vars.GetInt(4)
+	newVar := vars.GetInt(5)
+
+	switch data := obj.Data().(type) {
+	case marea.Vars:
+		if data.GetInt(off) == expected {
+			data.SetInt(newVar, off)
+			f.OperandStack.PushInt(1)
+		} else {
+			f.OperandStack.PushInt(0)
+		}
+	case []int32:
+		if data[offset] == expected {
+			data[offset] = newVar
+			f.OperandStack.PushInt(1)
+		} else {
+			f.OperandStack.PushInt(0)
+		}
+	default:
+		panic(fmt.Errorf("compareAndSwapInt, wrong type %T!", data))
+	}
+}
+
+// public native init getIntVolatile(Object obj, long l);
+// (Ljava/lang/Object;J)I
+func getIntVolatile(f *rtdt.Frame) {
+	obj := f.LocalVar.GetRef(1)
+	offset := f.LocalVar.GetLong(2)
+	off := uint(int32(offset))
+	//debug
+	fields := obj.Data()
+	fmt.Printf("LONG %d INT %d\n", offset, off)
+	fmt.Printf("OBJ %s\n", obj.Class().ClassName())
+	switch fields := fields.(type) {
+	case marea.Vars:
+		//debug
+		fmt.Printf("num %d\n", len(fields))
+		n := fields[off].Num
+		f.OperandStack.PushInt(n)
+	case []int32:
+		n := fields[off]
+		f.OperandStack.PushInt(n)
+	default:
+		panic(fmt.Errorf("getIntVolatile, wrong type %T!", fields))
 	}
 }
