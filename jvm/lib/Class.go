53f92dddf89d7c4a74f41ea9658ac4517802fe52
ray wang <wangrui@bongmi.com>
2017-12-18 22:33:58 +0800

    getDeclaredConstructor0, isAssignableFrom, isInterface, getModifiers, getSuperclass

--- jvm/lib/Class.go
+++ jvm/lib/Class.go
@@ -14,7 +14,12 @@ func init() {
 	register(utils.CLASSNAME_Class, "getName0", "()Ljava/lang/String;", getName0)
 	register(utils.CLASSNAME_Class, "forName0", "(Ljava/lang/String;ZLjava/lang/ClassLoader;Ljava/lang/Class;)Ljava/lang/Class;", forName0)
 	register(utils.CLASSNAME_Class, "getDeclaredFields0", "(Z)[Ljava/lang/reflect/Field;", getDeclaredFields0)
+	register(utils.CLASSNAME_Class, "getDeclaredConstructors0", "(Z)[Ljava/lang/reflect/Constructor;", getDeclaredConstructores0)
 	register(utils.CLASSNAME_Class, "isPrimitive", "()Z", isPrimitive)
+	register(utils.CLASSNAME_Class, "isAssignableFrom", "(Ljava/lang/Class;)Z", isAssignableFrom)
+	register(utils.CLASSNAME_Class, "isInterface", "()Z", isInterface)
+	register(utils.CLASSNAME_Class, "getModifiers", "()I", getModifiers)
+	register(utils.CLASSNAME_Class, "getSuperclass", "()Ljava/lang/Class;", getSuperclass)
 }
 
 // private static native boolean desiredAssertionStatus0(Class<?> clazz);
@@ -144,10 +149,142 @@ func isPrimitive(f *rtdt.Frame) {
 	}
 }
 
+// public native boolean isAssignableFrom(Class<?> cls);
+// (Ljava/lang/Class;)Z
+func isAssignableFrom(f *rtdt.Frame) {
+	thisClzObj := f.LocalVar.GetRef(0)
+	thisClz := thisClzObj.GetClzClass()
+	otherClzOjb := f.LocalVar.GetRef(1)
+	t := otherClzOjb.GetClzClass()
+
+	// check if is primitive class
+	for _, p := range cmn.PrimitiveNames {
+		if thisClz.ClassName() == p {
+			if thisClz == t {
+				f.OperandStack.PushInt(1)
+			} else {
+				f.OperandStack.PushInt(0)
+			}
+			return
+		}
+	}
+	if marea.IsDescandent(t, thisClz) {
+		f.OperandStack.PushInt(1)
+	} else {
+		f.OperandStack.PushInt(0)
+	}
+}
+
+// public native boolean isInterface();
+// ()Z
+func isInterface(f *rtdt.Frame) {
+	thisClzObj := f.LocalVar.GetRef(0)
+	clz := thisClzObj.GetClzClass()
+	if clz.IsInterface() {
+		f.OperandStack.PushInt(1)
+	} else {
+		f.OperandStack.PushInt(0)
+	}
+}
+
+// private native Constructor<T>[] getDeclaredConstructors0(boolean publicOnly);
+// (Z)[Ljava/lang/reflect/Constructor;
+func getDeclaredConstructores0(f *rtdt.Frame) {
+	thisClzObj := f.LocalVar.GetRef(0)
+	clz := thisClzObj.GetClzClass()
+	publicOnly := f.LocalVar.GetInt(1) > 0
+
+	constructors := make([]*marea.Method, 0, 4) // todo, 4 is bigger enough for most class
+	for _, m := range clz.MethodMap() {
+		if m.Name() == utils.METHODNAME_Init && (!publicOnly || m.IsPublic()) {
+			constructors = append(constructors, m)
+		}
+	}
+
+	loader := f.Method().Class().DefineLoader()
+	consClz := loader.Load(utils.CLASSNAME_Constructor)
+	consArrClz := loader.Load("[" + utils.CLASSNAME_Constructor)
+	consArr := marea.NewArrayA(consArrClz, int32(len(constructors)))
+	f.OperandStack.PushRef(consArr)
+	//clzClz := loader.Load(utils.CLASSNAME_Class)
+	clzArrClz := loader.Load("[" + utils.CLASSNAME_Class)
+	thread := f.Thread()
+	initM := consClz.GetMethodDirect(utils.METHODNAME_Init,
+		"(Ljava/lang/Class;"+
+			"[Ljava/lang/Class;"+
+			"[Ljava/lang/Class;"+
+			"II"+
+			"Ljava/lang/String;"+
+			"[B[B)V")
+
+	//Constructor(Class<T> declaringClass,
+	//	Class<?>[] parameterTypes,
+	//	Class<?>[] checkedExceptions,
+	//	int modifiers,
+	//	int slot,
+	//	String signature,
+	//	byte[] annotations,
+	//	byte[] parameterAnnotations)
+
+	for i, cons := range constructors {
+		consObj := marea.NewObject(consClz)
+		consArr.ArrGetRefs()[i] = consObj
+
+		ops := rtdt.NewOperandStack(9)
+		ops.PushRef(consObj)    // this
+		ops.PushRef(thisClzObj) // declaring class
+
+		// parameter types
+		pts := cons.ParameterTypes()
+		ptArray := marea.NewArrayA(clzArrClz, int32(len(pts)))
+		for i, p := range pts {
+			ptArray.ArrGetRefs()[i] = p.GetClassObject()
+		}
+		ops.PushRef(ptArray) // parameter Types
+
+		// exception types
+		eps := cons.ExceptionTypes()
+		epArray := marea.NewArray(clzArrClz, int32(len(eps)))
+		for i, e := range eps {
+			epArray.ArrGetRefs()[i] = e.GetClassObject()
+		}
+		ops.PushRef(epArray)             // checkExceptions
+		ops.PushInt(int32(cons.Flags())) // modifiers
+		ops.PushInt(int32(0))            // todo slotid
+		ops.PushRef(nil)                 // todo signature unsupported
+		ops.PushRef(nil)                 // todo anotations
+		ops.PushRef(nil)                 // todo parameterAnnotations
+
+		df := dummyFrame(ops, thread)
+		thread.PushFrame(df)
+		callMethod(initM, df)
+	}
+}
+
+//public native int getModifiers();
+//()I
+func getModifiers(f *rtdt.Frame) {
+	cls := f.LocalVar.GetRef(0)
+	flags := int32(cls.GetClzClass().GetFlags())
+	f.OperandStack.PushInt(flags)
+}
+
+//public native Class<? super T> getSuperclass();
+//()Ljava/lang/Class;
+func getSuperclass(f *rtdt.Frame) {
+	clsObj := f.LocalVar.GetRef(0)
+	clz := clsObj.GetClzClass()
+	superClz := clz.Superclass()
+	if superClz == nil {
+		f.OperandStack.PushRef(nil)
+	} else {
+		supClzObj := clz.Superclass().GetClassObject()
+		f.OperandStack.PushRef(supClzObj)
+	}
+}
+
 //private native Field[]       getDeclaredFields0(boolean publicOnly);
 
 //private native Method[]      getDeclaredMethods0(boolean publicOnly);
-
-//private native Constructor<T>[] getDeclaredConstructors0(boolean publicOnly);
 
 //private native Class<?>[]   getDeclaredClasses0();
