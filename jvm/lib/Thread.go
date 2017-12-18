4a0cdcf2fcb04c0f5c76988b343f0e5840e79d1f
ray wang <wangrui@bongmi.com>
2017-12-18 22:31:32 +0800

    setPriority0, isAlive, start0

--- jvm/lib/Thread.go
+++ jvm/lib/Thread.go
@@ -8,6 +8,9 @@ import (
 
 func init() {
 	register(utils.CLASSNAME_Thread, "currentThread", "()Ljava/lang/Thread;", currentThread)
+	register(utils.CLASSNAME_Thread, "setPriority0", "(I)V", setPriority0)
+	register(utils.CLASSNAME_Thread, "isAlive", "()Z", isAlive)
+	register(utils.CLASSNAME_Thread, "start0", "()V", start0)
 }
 
 // public static native currentThread ()Ljava/lang/Thread;
@@ -22,4 +25,30 @@ func currentThread(f *rtdt.Frame) {
 	currentT.SetRef(groupO, fieldG.VarIdx())
 	currentT.SetInt(1, fieldP.VarIdx())
 	f.OperandStack.PushRef(currentT)
+}
+
+// Todo, thread system
+// private native void setPriority0(int newPriority);
+// (I)V
+func setPriority0(frame *rtdt.Frame) {
+	// vars := frame.LocalVars()
+	// this := vars.GetThis()
+	// newPriority := vars.GetInt(1))
+}
+
+// TODO
+// public final native boolean isAlive();
+// ()Z
+func isAlive(frame *rtdt.Frame) {
+	//vars := frame.LocalVars()
+	//this := vars.GetThis()
+
+	stack := frame.OperandStack
+	stack.PushInt(0)
+}
+
+// TODO
+// public native void start0()
+// ()V
+func start0(frame *rtdt.Frame) {
 }
