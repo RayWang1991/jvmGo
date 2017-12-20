41a052b93deee1985707ff39b1c06c31ec068453
ray wang <wangrui@bongmi.com>
2017-12-20 23:31:56 +0800

    todo findSignal

--- jvm/lib/Signal.go
+++ jvm/lib/Signal.go
@@ -1,1 +1,23 @@
 package lib
+
+import (
+	"jvmGo/jvm/rtdt"
+	"fmt"
+	"jvmGo/jvm/marea"
+	"jvmGo/jvm/utils"
+)
+
+func init() {
+	register(utils.CLASSNAME_Signal, "findSignal", "(Ljava/lang/String;)I", findSignal)
+}
+
+// private static native int findSignal(String var0);
+// (Ljava/lang/String;)I
+
+func findSignal(f *rtdt.Frame) {
+	jstr := f.LocalVar.GetRef(0)
+	gostr := marea.GetGoString(jstr)
+	fmt.Printf("FINDSIGNAL %s\n", gostr)
+	f.OperandStack.PushInt(0)
+}
+
