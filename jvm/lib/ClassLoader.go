11de7b012fd092e3da3308ead27e65cae8253887
ray wang <wangrui@bongmi.com>
2017-12-22 10:29:15 +0800

    find loaded class, todo

--- jvm/lib/ClassLoader.go
+++ jvm/lib/ClassLoader.go
@@ -1,1 +1,21 @@
 package lib
+
+import (
+	"jvmGo/jvm/rtdt"
+	"jvmGo/jvm/marea"
+	"jvmGo/jvm/utils"
+)
+
+func init() {
+	register(utils.CLASSNAME_ClassLoader, "findLoadedClass0", "(Ljava/lang/String;)Ljava/lang/Class;", findLoadedClass0)
+}
+
+// private native final Class<?> findLoadedClass0(String name);
+// (Ljava/lang/String;)Ljava/lang/Class;
+func findLoadedClass0(f *rtdt.Frame) {
+	jstr := f.LocalVar.GetRef(1)
+	gostr := marea.GetGoString(jstr)
+	loader := f.Method().Class().DefineLoader() // todo
+	clz := loader.Load(gostr)
+	f.OperandStack.PushRef(clz.GetClassObject())
+}
