2c010f07cf72e379a4e5fbd606eaa816f07a04f7
ray wang <wangrui@bongmi.com>
2017-12-22 10:29:44 +0800

    todo getCachedURL

--- jvm/lib/URLClassPath.go
+++ jvm/lib/URLClassPath.go
@@ -1,1 +1,17 @@
 package lib
+
+import (
+	"jvmGo/jvm/rtdt"
+	"jvmGo/jvm/utils"
+)
+
+func init() {
+	register(utils.CLASSNAME_URLClassPath, "getLookupCacheURLs", "(Ljava/lang/ClassLoader;)[Ljava/net/URL;", getLookupCacheURLs)
+}
+
+// private static native URL[] getLookupCacheURLs(ClassLoader var0);
+// (Ljava/lang/ClassLoader;)[Ljava/net/URL;
+func getLookupCacheURLs(frame *rtdt.Frame) {
+	//TODO
+	frame.OperandStack.PushRef(nil)
+}
